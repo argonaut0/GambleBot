eea727a6f78a6be5d93c59472ca0db0227d605a8
Jason Hsu <jasonhsu2013@gmail.com>
2020-10-28 20:25:40 -0700

    Switch commands to library - Stash Commit

--- main.py
+++ main.py
@@ -1,52 +1,60 @@
-import discord
 import os
+from discord.ext import commands
 from blackjack import Blackjack
 from account import Account
-import keepalive
+
+COMMAND_PREFIX = "."
 
 token = os.environ.get("DISCORD_BOT_SECRET")
-client = discord.Client()
-
-# games is a dict {discord.Member : ABC game.Game}, contains all the games currently running
+# games is a dict {discord.Member.id : ABC game.Game}, contains all the games currently running
 games = {}
 # accounts is a dict {discord.Member.id : account.Account}
 accounts = {}
+bot = commands.Bot(command_prefix=COMMAND_PREFIX)
 
 
-# Runs on Login
-@client.event
+async def no_game_active(ctx):
+    return ctx.message.author.id not in games
+
+
+async def has_account(ctx):
+    return ctx.message.author.id in accounts
+
+
+async def not_has_account(ctx):
+    return ctx.message.author.id not in accounts
+
+
+@bot.event
 async def on_ready():
-    print("logged in as {0.user}".format(client))
+    print("logged in as {0.user}".format(bot))
 
 
-# Runs on send message
-@client.event
-async def on_message(message):
-    # Check so that the bot does not respond to itself
-    if message.author == client.user:
-        return
+@bot.command()
+@commands.check(no_game_active)
+@commands.check(has_account)
+async def blackjack(ctx, bet: int):
+    if accounts[ctx.author.id].valid(bet):
+        games[ctx.author] = Blackjack(ctx.author, accounts[ctx.author.id], bet)
+        await games[ctx.author].start(ctx)
 
-    # Check for active games
-    if message.author in games:
-        if not await games[message.author].play(message):
-            del games[message.author]
 
-    # Check for commands
-    if message.content.startswith("."):
-        # Create a new account if not existing
-        if message.author.id not in accounts:
-            accounts[message.author.id] = Account()
-        c = message.content[1:].split(" ")
-        if c[0] == "blackjack":
-            try:
-                if accounts[message.author.id].valid(int(c[1])):
-                    games[message.author] = Blackjack(message.author, accounts[message.author.id], int(c[1]))
-                    await games[message.author].start(message)
-            except ValueError:
-                return
-        if c[0] == "bal":
-            await message.channel.send(str(message.author) + " you have $" + str(accounts[message.author.id].cash))
+@bot.command()
+@commands.check(has_account)
+async def bal(ctx):
+    print("Checked a bal")
+    m = ctx.message
+    await m.channel.send(str(m.author) + " you have $" + str(accounts[m.author.id].cash))
+
+
+@bot.command()
+@commands.check(not_has_account)
+async def acc(ctx):
+    print("Created a new account")
+    m = ctx.message
+    accounts[m.author.id] = Account()
+    await m.channel.send(str(m.author) + " you have $" + str(accounts[m.author.id].cash))
 
 
 # keepalive.keep_alive()
-client.run(token)
+bot.run(token)
